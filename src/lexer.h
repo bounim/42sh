170e41a089ed5be7e54118b3ca4e46e7d7e4f489
Elouan Martinet <exa@elou.world>
2019-02-21 20:45:11 +0100

    Start lexer rework

--- src/lexer.h
+++ src/lexer.h
@@ -17,57 +17,50 @@
 # include <stdint.h>
 # include "libft.h"
 
-enum							e_lexer_state
-{
-	LEX_ST_GEN = 0,
-	LEX_ST_OP,
-	LEX_ST_QU,
-	LEX_ST_WD,
-	LEX_ST_BLK,
-	LEX_ST_BS,
-	LEX_ST_DLR,
-	LEX_ST_NB,
-};
-
 enum							e_lexer_type
 {
-	LEX_TP_WD = 0,
-	LEX_TP_OP,
+	TYPE_FOO = 0,
 };
 
 typedef struct s_lexer_token	t_lexer_token;
 
 struct							s_lexer_token
 {
-	enum e_lexer_type			type;
-	size_t						buffer_position;
-	uint8_t						*buffer;
-	size_t						size;
 	t_lexer_token				*previous;
 	t_lexer_token				*next;
+	enum e_lexer_type			type;
+	size_t						line_y; // TODO
+	size_t						line_x;
+	uint8_t						*buffer;
+	size_t						size;
 };
 
-/*
-** quote: quote type + quote type if != '\0'
-** When lexer exits, these can be set (0 = default / unset)
-** nomatch: cannot match a token
-** intoken: currently building a token
+typedef struct					s_heredoc
+{
+	int							skip_tabs; // 0 => <<, 1 => <<-
+	uint8_t						*delimiter;
+	size_t						delimiter_size;
+}								t_heredoc;
+
+/* FIXME
+** When lexer returns, these can be set (0 = default / unset)
+** impl_error: internal error, shouldn't happen
+** quoted: TODO
+** heredoc: TODO
 */
 
 typedef struct					s_lexer
 {
-	enum e_lexer_state			state;
-	enum e_lexer_state			bgstate;
 	t_lexer_token				*head;
 	t_lexer_token				*foot;
-	uint8_t						*buffer;
-	size_t						buffer_length;
+	uint8_t						*line;
+	size_t						line_size;
+	size_t						line_y;
 	size_t						i;
-	int							nomatch;
-	char						quote;
-	char						quotetype;
-	char						startqu;
-	char						intoken;
+	int							impl_error;
+	int							quoted;
+	t_heredoc					*heredoc_queue;
+	int							heredoc;
 }								t_lexer;
 
 /*
@@ -75,10 +68,12 @@ typedef struct					s_lexer
 ** assumes that this function has been called after each line return
 */
 
-uint8_t							lexer_check_line(uint8_t *buffer, size_t size);
+//uint8_t							lexer_check_line(uint8_t *buffer, size_t size);
 
-void							lexer_init(t_lexer *lex, uint8_t *buffer,
-		size_t buffer_length);
+void							lexer_init(t_lexer *lex, uint8_t *line,
+		size_t line_size);
+void							lexer_newline(t_lexer *lex, uint8_t *line,
+		size_t line_size);
 
 /*
 ** Returns -1 on error, 0 on success.
@@ -89,27 +84,5 @@ void							lexer_init(t_lexer *lex, uint8_t *buffer,
 
 int								lexer_read(t_lexer *lex);
 void							lexer_destroy(t_lexer *lex);
-
-/*
-** Token functions
-**
-** Return -1 on fatal error, 0 on success, 1 if it can't match a token.
-*/
-
-int								lexer_operator(t_lexer *lex);
-int								lexer_quote(t_lexer *lex);
-int								lexer_inquote(t_lexer *lex);
-int								lexer_backslash(t_lexer *lex);
-int								lexer_word(t_lexer *lex);
-int								lexer_blank(t_lexer *lex);
-
-/*
-** Internal functions
-** token: allocate a token node (linked list)
-*/
-
-int								lexer_token(t_lexer *lex, enum e_lexer_type);
-int								lexer_append(t_lexer *lex,
-		enum e_lexer_type type);
 
 #endif
--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -10,44 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lexer.h"
+#include "lexer_internal.h"
 
 #include <stdio.h> // XXX
 #include <unistd.h> // XXX
+
 static int			(*g_lexer_func[])(t_lexer *) = {
-	&lexer_operator,
-	&lexer_quote,
-	&lexer_inquote,
-	&lexer_backslash,
-	&lexer_blank,
-	&lexer_word,
+	unquoted_backslash_newline,
+	heredoc,
+	operator_append,
+	operator_end,
+	quoting,
+	expansion,
+	operator_new,
+	unquoted_blank,
+	word_append,
+	comment,
+	word_new,
 };
 
-void		printstate(enum e_lexer_state st)
-{
-	if (st == LEX_ST_GEN)
-		puts("ST_GEN");
-	if (st == LEX_ST_OP)
-		puts("ST_OP");
-	if (st == LEX_ST_BLK)
-		puts("ST_BLK");
-	if (st == LEX_ST_QU)
-		puts("ST_QU");
-	if (st == LEX_ST_BS)
-		puts("ST_BS");
-	if (st == LEX_ST_WD)
-		puts("ST_WD");
-	if (st == LEX_ST_DLR)
-		puts("ST_DLR");
-	if (st == LEX_ST_NB)
-		puts("ST_NB");
-}
-
-void				lexer_init(t_lexer *lex, uint8_t *buffer, size_t length)
+void				lexer_init(t_lexer *lex, uint8_t *line, size_t line_size)
 {
 	ft_memset(lex, 0, sizeof(*lex));
-	lex->buffer = buffer;
-	lex->buffer_length = length;
+	lex->line = line;
+	lex->line_size = line_size;
+}
+
+void				lexer_newline(t_lexer *lex, uint8_t *line, size_t line_size)
+{
+	lex->line = line;
+	lex->line_size = line_size;
+	lex->line_y++;
+	lex->i = 0;
 }
 
 int					lexer_read(t_lexer *lex)
@@ -56,12 +50,9 @@ int					lexer_read(t_lexer *lex)
 	int		r;
 
 	lex->i = 0;
-	while (lex->i < lex->buffer_length)
+	while (lex->i < lex->line_size)
 	{
 		f = 0;
-		printstate(lex->state);
-		write(1, (char *)(lex->buffer + lex->i), 1);
-		write(1, "\n", 1);
 		while (f < sizeof(g_lexer_func) / sizeof(g_lexer_func[0]))
 		{
 			if ((r = g_lexer_func[f](lex)) < 0)
@@ -72,22 +63,17 @@ int					lexer_read(t_lexer *lex)
 		}
 		if (f == sizeof(g_lexer_func) / sizeof(g_lexer_func[0]))
 		{
-			lex->nomatch = 1;
+			lex->impl_error = 1;
 			return (-1);
 		}
-		printf("lex quote = %d\n", lex->quote);
 		lex->i++;
 	}
-	printf("lex quote = %d\n", lex->quote);
-	if (lex->quote/* && (lex->state == LEX_ST_QU || lex->state == LEX_ST_BS)*/)
-	{
-		//lex->intoken = 1;
-		puts("missing quote");
-	}
-	return (0);
+	if (line_end(lex) < 0)
+		return (-1);
+	return (parser(lex));
 }
 
-int					lexer_token(t_lexer *lex, enum e_lexer_type type)
+int					token(t_lexer *lex, enum e_lexer_type type)
 {
 	t_lexer_token	*t;
 
@@ -99,9 +85,9 @@ int					lexer_token(t_lexer *lex, enum e_lexer_type type)
 		free(t);
 		return (-1);
 	}
-	t->buffer[0] = lex->buffer[lex->i];
+	t->buffer[0] = lex->line[lex->i];
 	t->size = 1;
-	t->buffer_position = lex->i;
+	t->line_x = lex->i;
 	t->type = type;
 	t->previous = lex->foot;
 	t->next = NULL;
@@ -110,7 +96,6 @@ int					lexer_token(t_lexer *lex, enum e_lexer_type type)
 	else
 		lex->head = t;
 	lex->foot = t;
-	lex->intoken = 1;
 	return (0);
 }
 
@@ -118,18 +103,21 @@ int					lexer_token(t_lexer *lex, enum e_lexer_type type)
 ** assumes a token exists (tests anyway)
 */
 
-int					lexer_append(t_lexer *lex, enum e_lexer_type type)
+int					append(t_lexer *lex, enum e_lexer_type type)
 {
 	uint8_t	*t;
 
 	if (!lex->foot)
+	{
+		lex->impl_error = 1;
 		return (-1);
+	}
 	if (!(t = malloc(lex->foot->size + 1)))
 		return (-1);
 	ft_memcpy(t, lex->foot->buffer, lex->foot->size);
 	free(lex->foot->buffer);
 	lex->foot->buffer = t;
-	lex->foot->buffer[lex->foot->size] = lex->buffer[lex->i];
+	lex->foot->buffer[lex->foot->size] = lex->line[lex->i];
 	lex->foot->size++;
 	lex->foot->type = type;
 	return (0);
@@ -150,4 +138,5 @@ void				lexer_destroy(t_lexer *lex)
 	}
 	lex->head = NULL;
 	lex->foot = NULL;
+	// TODO destroy parser?
 }
