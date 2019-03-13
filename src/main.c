def31e631778125f6b9823d21cbf806f0d70e378
Elouan Martinet <exa@elou.world>
2019-03-13 19:11:43 +0100

    Remove useless comments

--- src/main.c
+++ src/main.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 
 #include "twenty_one_sh.h"
-//schakor repo commit 27a3f3151872d9f639afd3b305d4cf71bd37456b
-//schakor repo commit 4ed25fab84387307a86d93d5b084a56919987b7d
 
 t_shell				g_shell;
 
