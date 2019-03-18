90cea08d902bd98f73bd7ac3993b1e03d763e6af
Elouan Martinet <exa@elou.world>
2019-03-18 17:17:58 +0100

    parser: replace recursion by loop

--- src/parser/parser_func.c
+++ src/parser/parser_func.c
@@ -13,15 +13,15 @@
 #include "parser.h"
 #include "lexer.h"
 
-static void		parser_add_cmd(t_parser_node **head, t_parser_node *new)
+static t_parser_node	**parser_add_cmd(t_parser_node **head)
 {
 	if (!(*head)->left)
-		parser_add_tree(&(*head)->left, new);
+		return (&(*head)->left);
 	else
-		parser_add_tree(&(*head)->right, new);
+		return (&(*head)->right);
 }
 
-static void		parser_add_operator(t_parser_node **head, t_parser_node *new)
+static t_parser_node	**parser_add_operator(t_parser_node **head, t_parser_node *new)
 {
 	if ((*head)->type == PARSER_COMMAND || new->type > (*head)->type)
 	{
@@ -31,37 +31,41 @@ static void		parser_add_operator(t_parser_node **head, t_parser_node *new)
 	else if (new->type <= (*head)->type)
 	{
 		if ((*head)->right && (*head)->right->type != PARSER_COMMAND)
-			parser_add_tree(&(*head)->right, new);
+			return (&(*head)->right);
 		else
 		{
 			new->left = (*head)->right;
 			(*head)->right = new;
 		}
 	}
+	return (NULL);
 }
 
 void			parser_add_tree(t_parser_node **head, t_parser_node *new)
 {
-	if (!*head)
+	while (head)
 	{
-		ft_putendl("adding x:");
-		if (new->type == PARSER_COMMAND && new->arg_head)
+		if (!*head)
+		{
+			ft_putendl("adding x:");
+			if (new->type == PARSER_COMMAND && new->arg_head)
+				print_token(new->arg_head->buffer->buf, new->arg_head->buffer->size);
+			else
+				ft_putnbr(new->type);
+			*head = new;
+			return ;
+		}
+		else if (new->type == PARSER_COMMAND)
+		{
+			ft_putendl("adding cmd:");
 			print_token(new->arg_head->buffer->buf, new->arg_head->buffer->size);
+			head = parser_add_cmd(head);
+		}
 		else
-			ft_putnbr(new->type);
-		*head = new;
-	}
-	else if (new->type == PARSER_COMMAND)
-	{
-		ft_putendl("adding cmd:");
-		print_token(new->arg_head->buffer->buf, new->arg_head->buffer->size);
-		parser_add_cmd(head, new);
-	}
-	else
-	{
-		ft_putendl("adding ope:");
-		// print_token(new->arg_head->buf, new->arg_head->size);
-		parser_add_operator(head, new);
+		{
+			ft_putendl("adding ope:");
+			head = parser_add_operator(head, new);
+		}
 	}
 }
 
