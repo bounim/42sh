1d21a4e40ebdf94cde2b258db450feb1216f26dd
Anteo GUILLOT <anteo.guillot@gmail.com>
2019-03-21 18:45:57 +0100

    ctrl+t done for readline. Drag letter now works

---
+++ src/readline/drag_char.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   drag_char.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguillot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/21 16:27:23 by aguillot          #+#    #+#             */
+/*   Updated: 2019/03/21 16:27:25 by aguillot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "twenty_one_sh.h"
+
+//ca prend le point char et ca le fait avancer d'une position. Si point_char->is_prompt alors ca prend le next. si t'es a la fin ca prend celui d'avant.
+
+void	switch_nodes(t_char *curr)
+{
+	t_char tmp;
+
+	if (!curr->next)
+		return ;
+	tmp = *curr;
+	ft_memmove(curr->charac, curr->next->charac, curr->next->len);
+	curr->len = curr->next->len;
+	ft_memmove(curr->next->charac, tmp.charac, tmp.len);
+	curr->next->len = tmp.len;
+}
+
+void	drag_char(void)
+{
+	t_char *curr;
+
+	curr = g_shell.edit.point_char;
+   	if (!curr || (curr->is_prompt == 1 && !curr->next))
+   		return ;
+   	if (curr->is_prompt == 0 && curr->prev->is_prompt == 1 && !curr->next)
+   	{
+   		move_right();
+   		return ;
+   	}
+   	if (curr == g_shell.edit.char_list.tail)
+   		curr = curr->prev;
+   	else if (curr->is_prompt == 1)
+   	{
+   		curr = curr->next;
+   		move_right();
+   	}
+   	switch_nodes(curr);
+   	clean_and_print();
+   	move_right();
+}
--- src/sh_defines.h
+++ src/sh_defines.h
@@ -27,7 +27,7 @@
 # define MODE_VI_INSERT		1
 # define MODE_VI_EDIT		2
 # define EDIT_MODE			3
-# define KEYMAP_SIZE		31
+# define KEYMAP_SIZE		32
 # define SEARCHMAP_SIZE		4
 # define TERM_SET			1
 #endif
