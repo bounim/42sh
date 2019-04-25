aa017f39f6758e8b019deb73b30fa14acce09ca1
Anteo GUILLOT <anteo.guillot@gmail.com>
2019-04-25 18:48:34 +0200

    BUILTIN: fixed another exit error message

--- src/builtin/built_exit.c
+++ src/builtin/built_exit.c
@@ -6,7 +6,7 @@
 /*   By: khsadira <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 15:18:44 by khsadira          #+#    #+#             */
-/*   Updated: 2019/04/15 14:54:58 by aguillot         ###   ########.fr       */
+/*   Updated: 2019/04/25 18:47:43 by aguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,44 @@ static void	print_exit_error_message(char *arg)
 
 static void	check_if_exit_arg_is_digit(char **arg)
 {
-	if (arg[1][0] == '-')
+	if (arg)
 	{
-		if (ft_strisdigit(arg[1] + 1) != 0)
-			print_exit_error_message(arg[1]);
+		if (!arg[1])
+			return ;
+		if (arg[1])
+		{
+			if (arg[1][0] == '-')
+			{
+				if (ft_strisdigit(arg[1] + 1) != 0)
+					print_exit_error_message(arg[1]);
+			}
+			else if (ft_strisdigit(arg[1]) != 0)
+				print_exit_error_message(arg[1]);
+		}
 	}
-	else if (ft_strisdigit(arg[1]) != 0)
-		print_exit_error_message(arg[1]);
+}
+
+static int	check_exit_arg_nb(char **arg)
+{
+	if (arg)
+	{
+		if (arg[1])
+		{
+			if (arg[2])
+			{
+				ft_putstr("sh: exit: too many arguments\n");
+				return (-1);
+			}
+		}
+	}
+	return (0);
 }
 
 int			built_exit(char **arg, t_envl *envl)
 {
 	(void)envl;
+	if (check_exit_arg_nb(arg) == -1)
+		return (-1);
 	file_from_history(g_shell.hist.history);
 	clean_shell();
 	ft_putstr("exit\n");
